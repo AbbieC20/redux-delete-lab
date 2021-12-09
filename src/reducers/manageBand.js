4c0990fd0bbe5048a281b0694f4962750b9bafb3
Abbie Cornelius <abbyc@sky.com>
2021-12-09 21:33:10 +0000

    commit tree e3651423cd595da0ebab4284efcf889e824e96ea

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,20 @@
+import uuid from 'uuid'
+
 export default function manageBand(state = {
-  bands: [],
+  bands: []
 }, action) {
-  switch(action.type) {
-    case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
-    default:
-      return state
-  
+  switch (action.type) {
+    case 'ADD_BAND':
+      const band = { 
+        id: uuid(),
+        name: action.name 
+      }
+      return { ...state, bands: [...state.bands, band] }
+    
+      case 'DELETE_BAND':
+        return { bands: state.bands.filter(x => x.id !== action.id) };
+    
+      default:
+      return state;
   }
 };
